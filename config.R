31b07b8f6d4e651691c82a919d5a373338270d1f
Juan Manuel Trak <jmtrak@MacBook-Pro-de-Juan-2.local>
2024-04-12 11:42:12 -0600

    First Commit

---
+++ config.R
@@ -0,0 +1,12 @@
+# Instalar las librerías para que estén en el archivo renv
+## Llamanr el paquete renv
+library(renv)
+
+## Instalar las librerias
+renv::install("dplyr")
+install("tidyveyrse")
+install("tidyr")
+install("pins")
+install("googlesheets4")
+install("rmarkdown")
+
